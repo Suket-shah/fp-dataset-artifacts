8d75be5ca753864bd3104d41e874b4ad1f3aa6db
Alex Chandler <alex3@Alexs-MacBook-Pro.local>
2021-11-23 21:40:49 -0600

    first draft of adv text generation

---
+++ generate_adv_examples.py
@@ -0,0 +1,41 @@
+
+import requests as r
+import re
+
+def get_common_words():
+    a_file = open("1000_common_words.txt", "r")
+    common_words = []
+    for line in a_file:
+        stripped_line = line.strip()
+        common_words.append(stripped_line)
+    a_file.close()
+    return(common_words)
+
+def get_adv_words(num_words=100):
+    # url ="https://raw.githubusercontent.com/fnielsen/afinn/master/afinn/data/AFINN-111.txt"
+    a_file = open("AFINN-111.txt", "r")
+    common_words = []
+    word_to_score={}
+    valid_cnt=0
+    error_cnt=0
+    for line in a_file:
+        stripped_line = line.strip()
+        score = re.findall('[-0-9]+', line)
+        score = str(score[0])
+        line_split = line.split()
+        word= line_split[0] 
+        score_2 = line_split[1]
+        score_2=str(score_2)
+        
+        if score_2==score:
+            word_to_score[word]=score
+            valid_cnt+=1
+        else:
+            error_cnt+=1
+        print(score_2)
+    num_words=int(num_words/2)
+    biggest_items = sorted(word_to_score.items(), reverse=True)[0:num_words]
+    smallest_items=sorted(word_to_score.items())[0:num_words]
+    charged_words = [x[0] for x in biggest_items] + [x[0] for x in smallest_items]
+    return charged_words
+
--- run.py
+++ run.py
@@ -5,12 +5,76 @@ from helpers import prepare_dataset_nli, prepare_train_dataset_qa, \
     prepare_validation_dataset_qa, QuestionAnsweringTrainer, compute_accuracy
 import os
 import json
+from generate_adv_examples import *
+import time 
 
 NUM_PREPROCESSING_WORKERS = 2
+def add_adversarial_text(example, adversarial_text):
+    example['context'] = example['context'] + adversarial_text
+    return example
 
+# Incorrectly Implemented
+def calc_attack_score(eval_args, dataset, string):
+    eval_kwargs, model, training_args, train_dataset_featurized, tokenizer, compute_metrics_and_store_predictions, prepare_eval_dataset, trainer_class= eval_args[0], eval_args[1], eval_args[2], eval_args[3], eval_args[4], eval_args[5], eval_args[6], eval_args[7]
+    # run model against subset of eval examples
+    # return score as a function of (or copy of) exact match or avg f1 score
+
+    modified_dataset = dataset.map(add_adversarial_text, fn_kwargs={"adversarial_text": string})
+    modified_eval_dataset_featurized = modified_dataset.map(
+            prepare_eval_dataset,
+            batched=True,
+            num_proc=NUM_PREPROCESSING_WORKERS,
+            remove_columns=modified_dataset.column_names
+        ) 
+    
+    # metric = datasets.load_metric('squad')
+    trainer = trainer_class(
+    model= model,
+    args=training_args,
+    train_dataset=train_dataset_featurized,
+    eval_dataset=modified_eval_dataset_featurized,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics_and_store_predictions
+    ) # TODO check parameters like if args is training_args
+        #TODO check if train_dataset_featurized needs to be sampled as well
+
+    results = trainer.evaluate(**eval_kwargs)
+    print('Evaluation results:')
+    print(results)
+
+    raise NotImplementedError()
+
+def generate_adv_examples(eval_args, dataset, desired_string_size, adv_vocab, beam_size):
+    # scenarios = [[x] for x in adv_vocab]
+    # scenarios starts for first itr starts out as size of adv_vocab, but every other iteration is size of beam
+    scenarios = ["" for x in range(adv_vocab)]
+    # add string to each item in scenarios
+    for i in range(desired_string_size):
+        for potential_word in adv_vocab:
+            new_scenarios = [] # contains (adv_text, attack_score) pairs
+            for scenario in scenarios:
+                if scenario == "":
+                    adv_text = potential_word
+                else:
+                    adv_text = scenario + " "+ potential_word
+                
+                attack_score = calc_attack_score(eval_args, dataset, adv_text)
+                new_scenarios.append((adv_text, attack_score))
+            scenarios = new_scenarios.sort(key = lambda x: x[1], reverse = True)[0:beam_size] # check if need reverse=True
+    return scenarios
+
+def add_adv_text_per_question(example, dictionary_mapping_example_to_index):
+    question = example["question"]
+    value = dictionary_mapping_example_to_index[question]
+    example["context"] = example["context"] + str(value) 
+    return example
 
 def main():
+
+    # while True:
+    #     i = i+1
     argp = HfArgumentParser(TrainingArguments)
+    # argp.add_argument('-f')
     # The HfArgumentParser object collects command-line arguments into an object (and provides default values for unspecified arguments).
     # In particular, TrainingArguments has several keys that you'll need/want to specify (when you call run.py from the command line):
     # --do_train
@@ -33,11 +97,11 @@ def main():
                       help="""This argument specifies the base model to fine-tune.
         This should either be a HuggingFace model ID (see https://huggingface.co/models)
         or a path to a saved model checkpoint (a folder containing config.json and pytorch_model.bin).""")
-    argp.add_argument('--task', type=str, choices=['nli', 'qa'], required=True,
+    argp.add_argument('--task', type=str, choices=['nli', 'qa'], default='qa',
                       help="""This argument specifies which task to train/evaluate on.
         Pass "nli" for natural language inference or "qa" for question answering.
         By default, "nli" will use the SNLI dataset, and "qa" will use the SQuAD dataset.""")
-    argp.add_argument('--dataset', type=str, default=None,
+    argp.add_argument('--dataset', type=str, default='squad',
                       help="""This argument overrides the default dataset used for the specified task.""")
     argp.add_argument('--max_length', type=int, default=128,
                       help="""This argument limits the maximum sequence length used during training/evaluation.
@@ -46,9 +110,11 @@ def main():
                       help='Limit the number of examples to train on.')
     argp.add_argument('--max_eval_samples', type=int, default=None,
                       help='Limit the number of examples to evaluate on.')
-
+    argp.add_argument('--gen_adv_examples', type=bool, default=True,
+                      help='Limit the number of examples to evaluate on.')
     training_args, args = argp.parse_args_into_dataclasses()
-
+    # print('training_args: : ', training_args)
+    # print('args: : ', args)
     # Dataset selection
     default_datasets = {'qa': ('squad',), 'nli': ('snli',)}
     dataset_id = tuple(args.dataset.split(':')) if args.dataset is not None else \
@@ -59,7 +125,9 @@ def main():
     eval_split = 'validation_matched' if dataset_id == ('glue', 'mnli') else 'validation'
     # Load the raw data
     dataset = datasets.load_dataset(*dataset_id)
-
+    dataset.save_to_disk("saved_dataset")
+    
+    
     # Here we select the right model fine-tuning head
     model_classes = {'qa': AutoModelForQuestionAnswering,
                      'nli': AutoModelForSequenceClassification}
@@ -100,6 +168,24 @@ def main():
         )
     if training_args.do_eval:
         eval_dataset = dataset[eval_split]
+        # dataset_to_dictionary =  {}
+        # for itr, example in enumerate(eval_dataset):
+        #     dataset_to_dictionary[example['question']] = itr
+        # key_0=eval_dataset[0]['question']
+        # key_1 = eval_dataset[1]['question']
+        # dataset_to_dictionary[key_0] = "Nazzis kill peoople"
+        # dataset_to_dictionary[key_1] = "Chinese communists"
+        # example 0 corresponds to Nazzis kill peoople and
+        # example 1 corresponds: Chine communists
+        # eval_dataset_with_adversarial_examples = eval_dataset.map(add_adv_text_per_question, fn_kwargs={"dictionary_mapping_example_to_index": dataset_to_dictionary})
+        # only_diff_contexts = []
+        # seen_contexts = set()
+        # for example in  eval_dataset:
+        #     if example['context'] in seen_contexts:
+        #         continue
+        #     only_diff_contexts.append(example)
+        #     seen_contexts.add(example['context'])
+
         if args.max_eval_samples:
             eval_dataset = eval_dataset.select(range(args.max_eval_samples))
         eval_dataset_featurized = eval_dataset.map(
@@ -107,22 +193,30 @@ def main():
             batched=True,
             num_proc=NUM_PREPROCESSING_WORKERS,
             remove_columns=eval_dataset.column_names
-        )
-
+        )  
     # Select the training configuration
     trainer_class = Trainer
     eval_kwargs = {}
     # If you want to use custom metrics, you should define your own "compute_metrics" function.
     # For an example of a valid compute_metrics function, see compute_accuracy in helpers.py.
-    compute_metrics = None
+    compute_metrics = lambda eval_preds: metric.compute(
+                predictions=eval_preds.predictions, references=eval_preds.label_ids)
     if args.task == 'qa':
         # For QA, we need to use a tweaked version of the Trainer (defined in helpers.py)
         # to enable the question-answering specific evaluation metrics
         trainer_class = QuestionAnsweringTrainer
         eval_kwargs['eval_examples'] = eval_dataset
         metric = datasets.load_metric('squad')
-        compute_metrics = lambda eval_preds: metric.compute(
-            predictions=eval_preds.predictions, references=eval_preds.label_ids)
+        if args.gen_adv_examples:
+            # makes the eval dataset only of length 100 when gen_adv_examples
+            eval_dataset = eval_dataset.select(range(100))
+            # for adv_example in adv_examples:
+            beam_size = 10
+            adversarial_words = set(get_common_words() + get_adv_words(100)) 
+        else:
+            compute_metrics = lambda eval_preds: metric.compute(
+                predictions=eval_preds.predictions, references=eval_preds.label_ids)
+        print("debug")
     elif args.task == 'nli':
         compute_metrics = compute_accuracy
     
@@ -156,7 +250,14 @@ def main():
         #   and https://huggingface.co/transformers/main_classes/callback.html#transformers.TrainerCallback
 
     if training_args.do_eval:
-        results = trainer.evaluate(**eval_kwargs)
+        if args.gen_adv_examples:
+            trainer_class = QuestionAnsweringTrainer
+            # arguments passed in that are necessary to test datasets with modified 'context' (where a certain adv example is appended to all 'context' in the modified dataset)
+            eval_args = [eval_kwargs, model, training_args, train_dataset_featurized, tokenizer, compute_metrics_and_store_predictions, prepare_eval_dataset,trainer_class]
+            adv_examples = generate_adv_examples(eval_args, eval_dataset, 5, adversarial_words, beam_size)
+            # TODO: Store generated adv_examples into file
+        else:
+            results = trainer.evaluate(**eval_kwargs)
 
         # To add custom metrics, you should replace the "compute_metrics" function (see comments above).
         #
@@ -192,3 +293,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def add_context(example, context_to_add):
+    example['sentence1'] = 'My sentence: ' + example['sentence1']
+    return example
+
+updated_dataset = dataset.map(add_context)
+updated_dataset['sentence1'][:5]
